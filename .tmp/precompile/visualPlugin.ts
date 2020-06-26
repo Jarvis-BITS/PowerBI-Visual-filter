d750aaa7e3ffa8d41dc0aefa6fc0bd332ebdccc2
Javin Bachani <javinben@gmail.com>
2020-06-26 08:07:58 +0530

    Added initial files

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,29 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api"
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+
+var visual217B56EDD81A4BE4A65CA8AF6A599C31_DEBUG: IVisualPlugin = {
+    name: 'visual217B56EDD81A4BE4A65CA8AF6A599C31_DEBUG',
+    displayName: 'visual',
+    class: 'Visual',
+    apiVersion: '2.6.0',
+    create: (options: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+
+        throw 'Visual instance not found';
+    },
+    custom: true
+};
+
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["visual217B56EDD81A4BE4A65CA8AF6A599C31_DEBUG"] = visual217B56EDD81A4BE4A65CA8AF6A599C31_DEBUG;
+}
+
+export default visual217B56EDD81A4BE4A65CA8AF6A599C31_DEBUG;
---
+++ README.md
@@ -0,0 +1,2 @@
+# PBI-Filter
+A custom visual developed for Microsoft PowerBI which filters report and provides robust output accordingly.
